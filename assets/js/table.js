ef9e066586ffab5efc793b85d139f87730da4cf3
AntonMM <antonio_moreno79@hotmail.com>
2017-06-07 12:49:14 +0200

    change structure and add jquery(JQuery is essencial)

--- assets/js/table.js
+++ assets/js/table.js
@@ -1,108 +1,73 @@
-// Version Testing
-// Antonio M. Mérida
-// 2017
+// prueba de examen "Test 1"
 
-var current_page = 0;
-var records_per_page = 2;
+var urlJson = "http://localhost/workProject/demonXJS/libs/listGames/snes/snes.json";
+var ajaxArray = Array();
+var numPage = 1; // variable para mostrar el numero de la pagina actual.
 
-// addEventListener in HTML
-document.getElementById("btn_next").addEventListener('click', nextPage, false); // next page
-document.getElementById("btn_prev").addEventListener('click', prevPage, false); // return page
+$(function() {
+    var forPage = 12; // varaible para mostrar un numero de elementos por pagina.
+    var dataActual = 0; // variable de elemento actual.
 
-// variable example
-var objJson = [
-    {
-        "titulosJuegos": "2020 Super Baseball",
-        // "publicaciones": "SNK",
-        "desarrolladores": "1993"
-    },
-    {
-        "titulosJuegos": "3 Ninjas Kick Back",
-        // "publicaciones": "Sony ImageSoft",
-        "desarrolladores": "1994"
-    },
-    {
-        "titulosJuegos": "3x3 Eyes - Juuma Houkan",
-        // "publicaciones": "Banpresto",
-        "desarrolladores": "1995"
-    },
-    {
-        "titulosJuegos": "3x3 Eyes - Seima Kourinden",
-        // "publicaciones": "Yutaka",
-        "desarrolladores": "1995"
-    },
-    {
-        "titulosJuegos": "'96 Zenkoku Koukou Soccer Senshuken",
-        // "publicaciones": "Mahou",
-        "desarrolladores": "22 de marzo de 1996 (Sólo Japón)"
-    },
-    {
-        "titulosJuegos": "7th Saga",
-        // "publicaciones": "Enix",
-        "desarrolladores": "Septiembre de 1993"
-    },
-    {
-        "titulosJuegos": "90 Minutes - European Prime Goal",
-       // "publicaciones": "Namco",
-        "desarrolladores": "1995 (Sólo Europa)"
-    },
-    {
-        "titulosJuegos": "Aaahh!!! Real Monsters",
-        // "publicaciones": "Majesco",
-        "desarrolladores": "Noviembre de 1995"
-    }
-]; // Can be obtained from another source, such as your objJson variable
+    $("#btn_next").click(function() {
+        mostrarDatos(dataActual, forPage, numPage+=1);
+    });
 
-function prevPage()
-{
-    if (current_page > 1) {
-        current_page--;
-        if (current_page == document.getElementById("page").innerText) {
-            current_page--;
+    $("#btn_prev").click(function() {
+        mostrarDatos(dataActual, forPage, numPage-=1);
+    }); 
+
+    $.ajax({
+        url: urlJson,
+        type: "GET", 
+        dataType: "json",
+        success: function (resultAjax) {
+            for(var i=0; i < resultAjax.results.length; i++) {
+                ajaxArray[i] = resultAjax.results[i].titulosJuegos + "::" + 
+                resultAjax.results[i].publicaciones + "::" +
+                resultAjax.results[i].desarrolladores;
+            }
+            // mostará los datos del array, desde el comienzo en "1".
+            mostrarDatos(dataActual, forPage, 1);
+
+            // regex(ajaxArray[7]);
+
+        },
+            // Si la petición falla
+        error: function (xhr, estado, error_producido) {
+            console.log("Error producido: " + error_producido);
+            console.log("Estado: " + estado);
+        },
+        //Tanto si falla como si funciona como sino funciona.
+        complete: function (xhr, estado) {
+            console.log("Peticion completa");
         }
-        changePage(current_page);
-    }
-}
+    });
+})
 
-function nextPage()
-{
-    if (current_page < numPages()) {
-        current_page++;
-        if (current_page == document.getElementById("page").innerText) {
-            current_page++;
-        }
-        changePage(current_page);
-
-    }
-}
+function mostrarDatos(dataActual, limitPage, page) {
+    var data = Array();
     
-function changePage(page)
-{
-    var btn_next = document.getElementById("btn_next");
-    var btn_prev = document.getElementById("btn_prev");
-    var listing_table = document.getElementById("listingTable");
-    var page_span = document.getElementById("page");
- 
-    // Validate page
-    if (page < 1) page = 1;
-    if (page > numPages()) page = numPages();
-
-    listing_table.innerHTML = "";
-
-    for (var i = (page-1) * records_per_page; i < (page * records_per_page); i++) {
-        listing_table.innerHTML += objJson[i].titulosJuegos + "<br>";        
+    for(dataActual; dataActual < limitPage; dataActual++) {
+        data = ajaxArray[dataActual].split("::");
+        $("#listingTable").append(data[0] + " >> " + data[1] + " >> " + data[2] + "</br>");
+    }
+    // al llegar al minimo.
+    if (numPage < 1) {
+        numPage = 1;
+        page = 1;
+    }
+    // a llegar al maximo.
+    if (numPage > ajaxArray.length) {
+        numPage = ajaxArray.length;
+        page = ajaxArray.length;        
     }
 
-
-    page_span.innerHTML = page;
+    $("#page").html(page);
 
 }
 
-function numPages()
-{
-    return Math.ceil(objJson.length / records_per_page);
+// funcion numero de paginas
+function numPages(num) {
+    return Math.ceil(num / forPage);
 }
 
-window.onload = function() {
-    changePage(1);
-};
